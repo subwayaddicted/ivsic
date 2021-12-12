9f9b8acd670f8f7e320898cd11111f5265685723
subwayaddicted <vlad.novitsky@hygge.software>
2021-12-13 01:00:36 +0200

    Added second signal

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from arguments import Arguments
 from signals import Signals
 
 arguments = Arguments([3, 2, 5, 4])
-signals = Signals(arguments)
+second_arguments = Arguments([2, 2, 1, 2])
+
+signals = Signals(arguments, second_arguments)
 signals.harmonic()
 signals.noise()
-signals.fourie_noise()
+signals.fourier_noise()
--- signals.py
+++ signals.py
@@ -8,9 +8,10 @@ import os
 
 
 class Signals:
-    def __init__(self, arguments: Arguments):
+    def __init__(self, arguments: Arguments, second_arguments: Arguments):
         self.formula = None
         self.arguments = arguments
+        self.second_arguments = second_arguments
         if not os.path.exists('images'):
             os.makedirs('images')
 
@@ -19,30 +20,53 @@ class Signals:
         return (self.arguments.before_first_sin * sin(self.arguments.inside_first_sin * pi * t)) + (
                 self.arguments.before_second_sin * sin(self.arguments.inside_second_sin * pi * t))
 
-    def harmonic(self):
-        t_array = arange(0, 10, 0.1)
-        y_array = []
+    def calculate_second_signal(self, t):
+        return (self.second_arguments.before_first_sin * sin(self.second_arguments.inside_first_sin * pi * t)) + (
+                self.second_arguments.before_second_sin * sin(self.second_arguments.inside_second_sin * pi * t))
+
+    def populate_harmonic_y_array(self, t_array: list, y_array: list, first: bool):
+        if y_array:
+            raise Exception('y_array is not empty, clear it')
 
         for t in t_array:
-            a = self.calculate_signal(t)
+            a = self.calculate_signal(t) if first else self.calculate_second_signal(t)
+            s = a + rand()
+            y_array.append(s)
+
+    def populate_fourier_noise_y_array(self, t_array: list, y_array: list, first: bool):
+        if y_array:
+            raise Exception('y_array is not empty, clear it')
+
+        for t in t_array:
+            a = self.calculate_signal(t) if first else self.calculate_second_signal(t)
             y_array.append(a)
 
+    def apply_fourier_transformation(self, y_array):
         fourie_y = fft(y_array)
-        y = abs(fourie_y)
+        return abs(fourie_y)
 
-        plain_x = arange(0, 10, 0.1)
-        plain_y = nsin(plain_x)
+    def harmonic(self):
+        t_array = arange(0, 10, 0.1)
+        first_y_array = []
+
+        self.populate_harmonic_y_array(t_array, first_y_array, True)
+        y = self.apply_fourier_transformation(first_y_array)
+
+        second_y_array = []
+        self.populate_harmonic_y_array(t_array, second_y_array, False)
+        second_y = self.apply_fourier_transformation(second_y_array)
 
         plot = Plot('harmonic')
 
         plot.make_fig()
-        plot.add_plot(t_array, y_array)
-        plot.add_plot(plain_x, plain_y)
+        plot.add_plot(t_array, first_y_array)
+        plot.add_plot(t_array, second_y_array)
         plot.save(1)
         plot.clear()
 
         plot.make_fig()
         plot.add_plot(t_array, y)
+        plot.add_plot(t_array, second_y)
         plot.save(2)
         plot.clear()
 
@@ -57,21 +81,21 @@ class Signals:
         plot.save(1)
         plot.clear()
 
-    def fourie_noise(self):
+    def fourier_noise(self):
         t_array = arange(0, 10, 0.01)
-        y_array = []
+        first_y_array = []
 
-        for t in t_array:
-            a = self.calculate_signal(t)
-            s = a + rand()
-            y_array.append(s)
+        self.populate_fourier_noise_y_array(t_array, first_y_array, True)
+        y = self.apply_fourier_transformation(first_y_array)
 
-        fourie_y = fft(y_array)
-        y = abs(fourie_y)
+        second_y_array = []
+        self.populate_fourier_noise_y_array(t_array, second_y_array, False)
+        second_y = self.apply_fourier_transformation(second_y_array)
 
-        plot = Plot('fourie_noise')
+        plot = Plot('fourier_noise')
 
         plot.make_fig()
         plot.add_plot(t_array, y)
+        plot.add_plot(t_array, second_y)
         plot.save(1)
         plot.clear()
